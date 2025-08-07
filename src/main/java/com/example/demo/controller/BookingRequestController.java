59c5979da1c701a26bf27661021356f813d14e1e
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-07 15:46:09 +0530

    Update BookingRequestController.java

--- src/main/java/com/example/demo/controller/BookingRequestController.java
+++ src/main/java/com/example/demo/controller/BookingRequestController.java
@@ -14,7 +14,7 @@ import com.example.demo.serv.TrainingServ;
 
 
 
-@CrossOrigin(origins = "http://localhost:3001") 
+@CrossOrigin(origins = "https://bweb-nu.vercel.app") 
 @RequestMapping("/api")
 @RestController
 public class BookingRequestController {
