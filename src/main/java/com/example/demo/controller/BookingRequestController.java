ca762af7abeab4e43cdf35fb1a851739a6f305cd
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-11 16:50:52 +0530

    Update BookingRequestController.java

--- src/main/java/com/example/demo/controller/BookingRequestController.java
+++ src/main/java/com/example/demo/controller/BookingRequestController.java
@@ -18,6 +18,9 @@ import com.example.demo.entity.TrainingEntity;
 import com.example.demo.serv.BookingRequestServ;
 import com.example.demo.serv.TrainingServ;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 
 @CrossOrigin(origins = "https://bweb-nu.vercel.app",
