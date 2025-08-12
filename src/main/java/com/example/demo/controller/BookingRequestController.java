6392b03e4682ebe94786a724163e12ce612d36e6
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-12 12:43:34 +0530

    Update BookingRequestController.java

--- src/main/java/com/example/demo/controller/BookingRequestController.java
+++ src/main/java/com/example/demo/controller/BookingRequestController.java
@@ -18,6 +18,7 @@ import com.example.demo.entity.TrainingEntity;
 import com.example.demo.serv.BookingRequestServ;
 import com.example.demo.serv.TrainingServ;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 
